26ace9fe1a21e82e715c1b53660b53d848781e61
axlCastill0 <axl.castilloblasco@gmail.com>
2023-09-25 14:09:41 -0400

    pygame general setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((1000, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    screen.fill((255, 0, 0))
+    clock.tick(60)
+    print(clock.tick())
