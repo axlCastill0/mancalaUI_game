41ffb11e1ba3913021feeef9cefb7838f9177cd3
axlCastill0 <axl.castilloblasco@gmail.com>
2023-09-27 20:37:06 -0400

    Fixed image placement

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Mancala - Axl Castillo / Thomas Journault")
 
 board_sprite = pygame.image.load("img/mancala_board.png").convert_alpha()
-
+board_rect = board_sprite.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
 run = True
 while run:
 
-    screen.blit(board_sprite, (100, 85))
+    screen.blit(board_sprite, board_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
