04db22ce483b7743b1d399f40bbb9e72337fba96
axlCastill0 <axl.castilloblasco@gmail.com>
2023-09-29 16:48:17 -0400

    Player move no cpu or rules

--- Mancala.py
+++ Mancala.py
@@ -10,7 +10,39 @@ class Mancala() :
         self.grid = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 
     def playerMove(self, id) :
-        return True
+        i = self.grid[id]
+        self.grid[id] = 0
+
+        sum = 0
+        grainesSum = 0
+        for a in range(6) :
+            grainesSum += self.grid[a]
+        if (grainesSum == 0) :
+            for j in range(7, 13) :
+                sum += self.grid[j]
+                self.grid[j] = 0
+            self.grid[13] = self.grid[13] + sum
+    
+        for x in range(i):
+            if id + x + 1 == 13:
+                x = x - 1
+            elif id + x + 1 > 13:
+                id = - x - 1
+                self.grid[id + x + 1] += 1
+            else:
+                self.grid[id + x + 1] += 1
+
+            if (x == i - 1) and (self.grid[id + x + 1] == 1) and ((id + x + 1) != 6) and ((id + x + 1) != 13) and (self.grid[self.opposite_pit_mapping[id + x + 1]] != 0):
+                if (id + x + 1 != 7) and (id + x + 1 != 8) and (id + x + 1 != 9) and (id + x + 1 != 10) and (id + x + 1 != 11) and (id + x + 1 != 12):
+                    self.grid[6] += 1
+                    self.grid[6] += self.grid[self.opposite_pit_mapping[id + x + 1]]
+                    self.grid[id + x + 1] = 0
+                    self.grid[self.opposite_pit_mapping[id + x + 1]] = 0
+                
+            if (x == i - 1) and (id + x + 1 == 6):
+                self.currentTurn = 0
+            else:
+                self.currentTurn = 1
     
     def cpuMove(self) :
         return True
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ pygame.display.set_caption("Mancala - Axl Castillo / Thomas Journault")
 board_img = pygame.image.load("img/mancala_board.png").convert_alpha()
 board_rect = board_img.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-red_pit_e = pygame.image.load("img/red_empty.png")
+red_pit_0 = pygame.image.load("img/red_empty.png")
 red_pit_1 = pygame.image.load("img/red_1.png")
 red_pit_2 = pygame.image.load("img/red_2.png")
 red_pit_3 = pygame.image.load("img/red_3.png")
@@ -33,7 +33,7 @@ red_pit_5 = pygame.image.load("img/red_5.png")
 red_pit_6 = pygame.image.load("img/red_6.png")
 red_pit_f = pygame.image.load("img/red_full.png")
 
-red_point_e = pygame.image.load("img/rpoint_empty.png")
+red_point_0 = pygame.image.load("img/rpoint_empty.png")
 red_point_1 = pygame.image.load("img/rpoint_1.png")
 red_point_2 = pygame.image.load("img/rpoint_2.png")
 red_point_3 = pygame.image.load("img/rpoint_3.png")
@@ -46,7 +46,7 @@ red_point_9 = pygame.image.load("img/rpoint_9.png")
 red_point_10 = pygame.image.load("img/rpoint_10.png")
 red_point_f = pygame.image.load("img/rpoint_full.png")
 
-blue_pit_e = pygame.image.load("img/blue_empty.png")
+blue_pit_0 = pygame.image.load("img/blue_empty.png")
 blue_pit_1 = pygame.image.load("img/blue_1.png")
 blue_pit_2 = pygame.image.load("img/blue_2.png")
 blue_pit_3 = pygame.image.load("img/blue_3.png")
@@ -55,7 +55,7 @@ blue_pit_5 = pygame.image.load("img/blue_5.png")
 blue_pit_6 = pygame.image.load("img/blue_6.png")
 blue_pit_f = pygame.image.load("img/blue_full.png")
 
-blue_point_e = pygame.image.load("img/bpoint_empty.png")
+blue_point_0 = pygame.image.load("img/bpoint_empty.png")
 blue_point_1 = pygame.image.load("img/bpoint_1.png")
 blue_point_2 = pygame.image.load("img/bpoint_2.png")
 blue_point_3 = pygame.image.load("img/bpoint_3.png")
@@ -68,6 +68,54 @@ blue_point_9 = pygame.image.load("img/bpoint_9.png")
 blue_point_10 = pygame.image.load("img/bpoint_10.png")
 blue_point_f = pygame.image.load("img/bpoint_full.png")
 
+red_pit_img_array = []
+red_pit_img_array.append(red_pit_0)
+red_pit_img_array.append(red_pit_1)
+red_pit_img_array.append(red_pit_2)
+red_pit_img_array.append(red_pit_3)
+red_pit_img_array.append(red_pit_4)
+red_pit_img_array.append(red_pit_5)
+red_pit_img_array.append(red_pit_6)
+red_pit_img_array.append(red_pit_f)
+
+blue_pit_img_array = []
+blue_pit_img_array.append(blue_pit_0)
+blue_pit_img_array.append(blue_pit_1)
+blue_pit_img_array.append(blue_pit_2)
+blue_pit_img_array.append(blue_pit_3)
+blue_pit_img_array.append(blue_pit_4)
+blue_pit_img_array.append(blue_pit_5)
+blue_pit_img_array.append(blue_pit_6)
+blue_pit_img_array.append(blue_pit_f)
+
+red_point_img_array = []
+red_point_img_array.append(red_point_0)
+red_point_img_array.append(red_point_1)
+red_point_img_array.append(red_point_2)
+red_point_img_array.append(red_point_3)
+red_point_img_array.append(red_point_4)
+red_point_img_array.append(red_point_5)
+red_point_img_array.append(red_point_6)
+red_point_img_array.append(red_point_7)
+red_point_img_array.append(red_point_8)
+red_point_img_array.append(red_point_9)
+red_point_img_array.append(red_point_10)
+red_point_img_array.append(red_point_f)
+
+blue_point_img_array = []
+blue_point_img_array.append(blue_point_0)
+blue_point_img_array.append(blue_point_1)
+blue_point_img_array.append(blue_point_2)
+blue_point_img_array.append(blue_point_3)
+blue_point_img_array.append(blue_point_4)
+blue_point_img_array.append(blue_point_5)
+blue_point_img_array.append(blue_point_6)
+blue_point_img_array.append(blue_point_7)
+blue_point_img_array.append(blue_point_8)
+blue_point_img_array.append(blue_point_9)
+blue_point_img_array.append(blue_point_10)
+blue_point_img_array.append(blue_point_f)
+
 pit_array = []
 
 pit0 = Pit.Pit(0, PIT_WIDTH_COORDS[0], BLUE_PIT_HEIGHT, 4, blue_pit_4)
@@ -76,14 +124,14 @@ pit2 = Pit.Pit(2, PIT_WIDTH_COORDS[2], BLUE_PIT_HEIGHT, 4, blue_pit_4)
 pit3 = Pit.Pit(3, PIT_WIDTH_COORDS[3], BLUE_PIT_HEIGHT, 4, blue_pit_4)
 pit4 = Pit.Pit(4, PIT_WIDTH_COORDS[4], BLUE_PIT_HEIGHT, 4, blue_pit_4)
 pit5 = Pit.Pit(5, PIT_WIDTH_COORDS[5], BLUE_PIT_HEIGHT, 4, blue_pit_4)
-pit6 = Pit.Pit(6, POINT_PIT_WIDTH[0], POINT_PIT_HEIGHT, 0, blue_point_e)
+pit6 = Pit.Pit(6, POINT_PIT_WIDTH[0], POINT_PIT_HEIGHT, 0, blue_point_0)
 pit7 = Pit.Pit(7, PIT_WIDTH_COORDS[5], RED_PIT_HEIGHT, 4, red_pit_4)
 pit8 = Pit.Pit(8, PIT_WIDTH_COORDS[4], RED_PIT_HEIGHT, 4, red_pit_4)
 pit9 = Pit.Pit(9, PIT_WIDTH_COORDS[3], RED_PIT_HEIGHT, 4, red_pit_4)
 pit10 = Pit.Pit(10, PIT_WIDTH_COORDS[2], RED_PIT_HEIGHT, 4, red_pit_4)
 pit11 = Pit.Pit(11, PIT_WIDTH_COORDS[1], RED_PIT_HEIGHT, 4, red_pit_4)
 pit12 = Pit.Pit(12, PIT_WIDTH_COORDS[0], RED_PIT_HEIGHT, 4, red_pit_4)
-pit13 = Pit.Pit(13, POINT_PIT_WIDTH[1], POINT_PIT_HEIGHT, 0, red_point_e)
+pit13 = Pit.Pit(13, POINT_PIT_WIDTH[1], POINT_PIT_HEIGHT, 0, red_point_0)
 
 pit_array.append(pit0)
 pit_array.append(pit1)
@@ -104,6 +152,20 @@ pits = Mancala_UI.Pits(pit_array)
 
 mancala = Mancala.Mancala([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0])
 
+def updateGameInterface() :
+    grid = mancala.grid
+    for i in range(7) :
+        if (i == 6) :
+            pits.pits[i].img = blue_point_img_array[grid[i]]
+        else :
+            pits.pits[i].img = blue_pit_img_array[grid[i]]
+
+    for i in range(7, 13) :
+        if (i == 13) :
+            pits.pits[i].img = red_point_img_array[grid[i]]
+        else :
+            pits.pits[i].img = red_pit_img_array[grid[i]]
+
 run = True
 while run:
     
@@ -112,17 +174,20 @@ while run:
     pits.draw(screen)
 
     if pit0.action() == True :
-        print(pit0.seeds)
+        mancala.playerMove(0)
     if pit1.action() == True :
-        print(pit1.seeds)
+        mancala.playerMove(1)
     if pit2.action() == True :
-        print(pit2.seeds)
+        mancala.playerMove(2)
     if pit3.action() == True :
-        print(pit3.seeds)
+        mancala.playerMove(3)
     if pit4.action() == True :
-        print(pit4.seeds)
+        mancala.playerMove(4)
     if pit5.action() == True :
-        print(pit5.seeds)
+        mancala.playerMove(5)
+        print(mancala.grid)
+
+    updateGameInterface()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
